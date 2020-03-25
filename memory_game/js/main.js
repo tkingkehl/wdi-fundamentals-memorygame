3396f9e21dc49b324f6fbde93c56f9ff785269be
Tim King-Kehl <acanceroustwzlr@gmail.com>
2020-03-25 18:41:33 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
-console.log("Up and running!");
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cardOne = cards[0];
+let cardTwo = cards[2];
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardOne);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2)
+{
+	if (cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else 
+	{
+		alert("Sorry, try again");
+	}
+}
+
+
+// for (let i = 0; i < cardsInPlay.length; i++)
+// {
+// 	console.log("User flipped " + cardsInPlay[i]);
+// }
