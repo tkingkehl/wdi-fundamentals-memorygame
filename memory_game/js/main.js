6ba7f4c9364d71629453cc08c3943d1321bbd353
Tim King-Kehl <acanceroustwzlr@gmail.com>
2020-03-25 18:12:24 -0500

    added basic html, css, and game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "king";
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardOne);
