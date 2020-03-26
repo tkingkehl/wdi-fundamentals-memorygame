de5a86924e9d4642dff9bce5403ec73b2a6a8908
Tim King-Kehl <acanceroustwzlr@gmail.com>
2020-03-25 19:25:53 -0500

    created game logic with objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,51 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+]
+
+
+
 let cardsInPlay = [];
 
 function checkForMatch()
 {
 	if (cardsInPlay[0] === cardsInPlay[1])
 		{
+			//alert("You found a match!");
 			console.log("You found a match!");
 		}
 		else 
 		{
+			//alert("Sorry, try again");
 			console.log("Sorry, try again");
 		}
 }
 
 function flipCard(cardId)
 {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
 	if (cardsInPlay.length === 2)
 	{
